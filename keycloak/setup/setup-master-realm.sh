1370ced32698e0df2cfaf3e343bdcc505f0f813f
lotyp <lotyp7@gmail.com>
2023-03-28 17:48:18 +0300

    feat: extended keycloak configuration

--- keycloak/setup/setup-master-realm.sh
+++ keycloak/setup/setup-master-realm.sh
@@ -7,8 +7,8 @@ KCADM=/opt/keycloak/bin/kcadm.sh
 ${KCADM} config credentials --server "$KC_INTERNAL_HOSTNAME_URL" --realm master --user "$KEYCLOAK_ADMIN" --password "$KEYCLOAK_ADMIN_PASSWORD"
 
 ${KCADM} update realms/master \
-    -s 'displayName=w*d auth' \
-    -s 'displayNameHtml=<img src="https://wayof.dev/assets/logo.gh-light-mode-only.png" width="148">'
+    -s 'displayName="Master Login"' \
+    -s loginTheme=keywind
 
 REALM_MASTER_FILE=/tmp/setup/realms/realm-master.json
 if [ -f $REALM_MASTER_FILE ]; then
--- keycloak/setup/setup-operations-realm.sh
+++ keycloak/setup/setup-operations-realm.sh
@@ -9,13 +9,20 @@ ${KCADM} config credentials --server "$KC_INTERNAL_HOSTNAME_URL" --realm master 
 ${KCADM} create realms \
     -s "realm=${OIDC_ADMIN_REALM}" \
     -s enabled=true \
-    -s 'displayName="Company Employees"' \
+    -s displayName="${OIDC_ADMIN_REALM_DISPLAY_NAME}" \
     -s accessTokenLifespan=300 \
     -s ssoSessionIdleTimeout=3600 \
     -s ssoSessionMaxLifespan=3600 \
     -s revokeRefreshToken=true \
     -s refreshTokenMaxReuse=0 \
-    -s defaultSignatureAlgorithm=RS256
+    -s defaultSignatureAlgorithm=RS256 \
+    -s editUsernameAllowed=false \
+    -s registrationEmailAsUsername=true \
+    -s registrationAllowed=false \
+    -s resetPasswordAllowed=true \
+    -s rememberMe=true \
+    -s duplicateEmailsAllowed=false \
+    -s loginTheme=keywind
 
 ${KCADM} create clients -r ${OIDC_ADMIN_REALM} -f - <<EOF
 {
@@ -23,7 +30,8 @@ ${KCADM} create clients -r ${OIDC_ADMIN_REALM} -f - <<EOF
     "clientId": "${OIDC_ADMIN_APP_ID}",
     "directAccessGrantsEnabled": true,
     "webOrigins": ["*"],
-    "standardFlowEnabled": false,
+    "redirectUris": ["*"],
+    "standardFlowEnabled": true,
     "clientAuthenticatorType": "client-secret",
     "secret": "${OIDC_ADMIN_APP_SECRET}",
     "bearerOnly": false,
@@ -42,8 +50,9 @@ ${KCADM} create clients -r ${OIDC_ADMIN_REALM} -f - <<EOF
     "clientId": "${OIDC_ADMIN_USER_ID}",
     "directAccessGrantsEnabled": true,
     "webOrigins": ["*"],
+    "redirectUris": ["*"],
     "bearerOnly": false,
-    "standardFlowEnabled": false,
+    "standardFlowEnabled": true,
     "publicClient": true,
     "serviceAccountsEnabled": false,
     "authorizationServicesEnabled": false,
@@ -54,14 +63,14 @@ ${KCADM} create clients -r ${OIDC_ADMIN_REALM} -f - <<EOF
 EOF
 
 ### Users
-${KCADM} create users -r ${OIDC_ADMIN_REALM} -s enabled=true -s emailVerified=true -s username=john.doe -s email=john.doe@wayof.dev -s firstName=John -s lastName=Doe
-${KCADM} set-password -r ${OIDC_ADMIN_REALM} --username john.doe --new-password john.doe
+${KCADM} create users -r ${OIDC_ADMIN_REALM} -s enabled=true -s emailVerified=true -s username=john.doe@wayof.dev -s email=john.doe@wayof.dev -s firstName=John -s lastName=Doe
+${KCADM} set-password -r ${OIDC_ADMIN_REALM} --username john.doe@wayof.dev --new-password john.doe
 
-${KCADM} create users -r ${OIDC_ADMIN_REALM} -s enabled=true -s emailVerified=true -s username=jane.doe -s email=jane.doe@wayof.dev -s firstName=Jane -s lastName=Doe
-${KCADM} set-password -r ${OIDC_ADMIN_REALM} --username jane.doe --new-password jane.doe
+${KCADM} create users -r ${OIDC_ADMIN_REALM} -s enabled=true -s emailVerified=true -s username=jane.doe@wayof.dev -s email=jane.doe@wayof.dev -s firstName=Jane -s lastName=Doe
+${KCADM} set-password -r ${OIDC_ADMIN_REALM} --username jane.doe@wayof.dev --new-password jane.doe
 
-${KCADM} create users -r ${OIDC_ADMIN_REALM} -s enabled=true -s emailVerified=true -s username=johnny.doe -s email=johnny.doe@wayof.dev -s firstName=Johnny -s lastName=Doe
-${KCADM} set-password -r ${OIDC_ADMIN_REALM} --username johnny.doe --new-password johnny.doe
+${KCADM} create users -r ${OIDC_ADMIN_REALM} -s enabled=true -s emailVerified=true -s username=johnny.doe@wayof.dev -s email=johnny.doe@wayof.dev -s firstName=Johnny -s lastName=Doe
+${KCADM} set-password -r ${OIDC_ADMIN_REALM} --username johnny.doe@wayof.dev --new-password johnny.doe
 
 ### Roles
 ${KCADM} add-roles \
